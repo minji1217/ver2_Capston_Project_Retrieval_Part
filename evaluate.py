ef475e267bda561bc2de22e13a8e88593dabc907
Minji <wjdalswl6756@naver.com>
2026-05-04 01:18:13 +0900

    for 600

--- evaluate.py
+++ evaluate.py
@@ -2,7 +2,8 @@
 import numpy as np
 import config
 
-def calculate_metrics(predicted_ids, gt_ids, k_list = [50, 100, 150]):
+# def calculate_metrics(predicted_ids, gt_ids, k_list = [50, 100, 150]):
+def calculate_metrics(predicted_ids, gt_ids, k_list = [50, 100, 150, 600]):
     
     # predicted_ids : RRF Score 통해 최종 top-k(100) 결과 리스트 
     # gt_ids : 실제 저자가 인용한 정답 타겟 논문 ID 리스트 
